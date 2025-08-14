a080974587edf87dbd9d034245c1755c128adc50
clenio <mouraaf@hotmail.com>
2025-08-14 00:02:05 -0300

    Build: restaura arquivos CRA essenciais (index.js/index.css/reportWebVitals.js) e setupTests

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 Busca Dinâmica 2.0</p>
+    </footer>
+  );
+}
+
+export default Footer;
